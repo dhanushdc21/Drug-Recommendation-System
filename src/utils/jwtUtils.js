747fbf0effe6f96fbb2b5ad53cf04379b6593aad
dhanushdc21 <chandrandhanush369@gmail.com>
2024-05-11 12:41:29 +0530

    Second Commit : Fixed JWTTOKEN error

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,12 +1,25 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-dotenv.config({ path:'../.env'});
-console.log('Secret Key :', process.env.SECRET_KEY)
+import { fileURLToPath } from 'url';
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const envPath = path.resolve(__dirname, '../../.env');
+
+try {
+    dotenv.config({ path: envPath });
+    console.log('Environment variables loaded successfully');
+    console.log('Secret Key:', process.env.SECRET_KEY);
+} catch (err) {
+    console.error('Error loading .env file:', err.message);
+}
+
 const secretKey = process.env.SECRET_KEY;
 
-
 const generateJWTToken = (payload) => {
+    console.log('Secret Key in generateJWTToken:', secretKey); // Add this line
     if (!secretKey) {
+        console.error('Secret key is not defined'); // Add this line
         throw new Error('Secret key is not defined');
     }
 
